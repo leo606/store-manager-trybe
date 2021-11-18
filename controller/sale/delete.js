0160464f61a36034ad1d53bfb6e0fe41d9270e28
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-11-18 18:59:19 -0300

    requisito 8

--- controller/sale/delete.js
+++ controller/sale/delete.js
@@ -1,5 +1,19 @@
+const service = require('../../service/sale');
 const statusCodes = require('../statusCodes.json');
 
-module.exports = async (_req, res, _next) => {
-  res.status(statusCodes.notImplemented).end();
+module.exports = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const deleted = await service.remove(id);
+
+    if (deleted.err) {
+      return next({
+        err: { code: deleted.err.code, message: deleted.err.message },
+        status: statusCodes.unprocessableEntity,
+      });
+    }
+    res.status(statusCodes.ok).json(deleted);
+  } catch (e) {
+    console.log(e);
+  }
 };
--- service/sale/delete.js
+++ service/sale/delete.js
@@ -1,3 +1,22 @@
-module.exports = async () => {
-  
+const { ObjectID } = require('mongodb');
+const sale = require('../../model/document')('sales');
+
+const ERR_OBJ = { err: { code: 'invalid_data', message: 'Wrong sale ID format' } };
+
+module.exports = async (id) => {
+  try {
+    if (!ObjectID.isValid(id)) {
+      return ERR_OBJ;
+    }
+
+    const saleToDelete = await sale.find(new ObjectID(id));
+    if (!saleToDelete) {
+      return ERR_OBJ;
+    }
+
+    await sale.remove(id);
+    return saleToDelete;
+  } catch (e) {
+    console.log(e);
+  }
 };
