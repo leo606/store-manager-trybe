3751f154af2c349717c27e2d41650a579624e054
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-11-18 17:32:26 -0300

    update model return new document

--- model/document/update.js
+++ model/document/update.js
@@ -4,9 +4,12 @@ const connection = require('../connection');
 module.exports = async (collection, entity) => {
   const { id, ...dataToSet } = entity;
 
-  const updated = await (await connection()).collection(collection).updateOne(
-    { _id: new ObjectID(id) },
-    { $set: { ...dataToSet } },
+  const updated = await (await connection())
+    .collection(collection)
+    .findOneAndUpdate(
+      { _id: new ObjectID(id) },
+      { $set: { ...dataToSet } },
+      { returnDocument: 'after' }, // source: https://stackoverflow.com/questions/24747189/update-and-return-document-in-mongodb
   );
 
   return updated;
