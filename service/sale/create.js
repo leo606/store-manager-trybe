b811dbd8487973a721720874d584dc5f720308fd
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-11-18 15:48:25 -0300

    requisito 5

--- service/sale/create.js
+++ service/sale/create.js
@@ -1,11 +1,13 @@
+const sale = require('../../model/document')('sales');
 const { saleSchema } = require('../joiSchemas');
 
 const ERR_OBJ = { err: { message: 'Wrong product ID or invalid quantity', code: 'invalid_data' } };
 
-module.exports = async (sale) => {
-  const valid = saleSchema.validate(sale);
+module.exports = async (products) => {
+  const valid = saleSchema.validate(products);
   if (valid.error) {
     return ERR_OBJ;
   }
-  return {};
+  const insert = await sale.create({ itensSold: products });
+  return insert.ops[0];
 };
