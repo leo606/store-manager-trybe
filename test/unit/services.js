7e3f7bc5eafd0169255c0eda9e6477656bea7734
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-11-24 22:32:45 -0300

    service test

--- test/unit/services.js
+++ test/unit/services.js
@@ -4,34 +4,26 @@ const { expect } = require("chai");
 const product = require("../../models/document")("products");
 const service = require("../../services/product");
 
-const productMock = {
-  name: "product one",
-  quantity: 90,
-};
+describe("test products service", () => {
+  const mockProduct = { _id: "lje", name: "product name", quantity: 9 };
 
-describe("test products", () => {
   before(() => {
-    const dbCreateResp = {
-      ops: [
-        {
-          _id: {
-            $oid: "6198f2906e81d128301a5052",
-          },
-          name: "product one",
-          quantity: 90,
-        },
-      ],
-    };
-
-    sinon.stub(product, "create").resolves(dbCreateResp);
+    sinon.stub(product, "list").resolves(mockProduct);
   });
 
-  describe("inserir um product", () => {
-    it("quando o produto está ok", async () => {
-      console;
-      const ret = await service.create(productMock);
-
-      expect("ret").to.be.an("string");
+  describe("inserir um produto invalido", () => {
+    it("name e quantity invalidos", async () => {
+      const res = await service.create({ name: "sdf", quantity: "2" });
+      expect(res)
+        .to.be.an("object")
+        .that.have.property("err")
+        .that.have.property("code", "invalid_data");
     });
   });
+  it("produto criar produto existente", async () => {
+    const res = await service.list();
+    expect(res).to.be.an("object");
+  });
+
+  describe("inserir um produto ja existente", () => {});
 });
